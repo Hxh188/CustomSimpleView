379e4f8be4d22221a2a179f4de819e73d00ddfa3
huangxiaohui <hxiaohui0@linewell.com>
2019-11-28 23:03:12 +0800

    1.新增基础drawable

---
+++ basicdrawable/src/main/java/com/hxh/simpleview_lib/basicdrawable/TestBasicDrawActivity.java
@@ -0,0 +1,19 @@
+package com.hxh.simpleview_lib.basicdrawable;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @author huangxiaohui
+ * @date 2019-11-28
+ */
+public class TestBasicDrawActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.basicdra_test);
+    }
+}
